a3bd951ac400f2a78466c9451ecccd3119e87f83
gbogopolsky <gbogo19@gmail.com>
2019-04-20 16:23:20 +0200

    Intrinsic version of saxpy.

---
+++ ExampleOptimisation/3-Saxpy/main_intrinsics.cpp
@@ -0,0 +1,78 @@
+/***************************************
+	Auteur : Pierre Aubert
+	Mail : aubertp7@gmail.com
+	Licence : CeCILL-C
+****************************************/
+
+//AVX intrinsic functions
+#include <immintrin.h>
+
+#include <iostream>
+#include "asterics_hpc.h"
+
+using namespace std;
+
+///Do the Hadamard product
+/**	@param[out] tabResult : table of results of tabX*tabY
+ * 	@param scal : multiplication scalar (a)
+ * 	@param tabX : input table
+ * 	@param tabY : input table
+ * 	@param nbElement : number of elements in the tables
+*/
+void saxpy(float* tabResult, float scal, const float* tabX, const float* tabY, long unsigned int nbElement) {
+    __m256 vecScal = _mm256_broadcast_ss(&scal);
+    long unsigned int vecSize = VECTOR_ALIGNEMENT/sizeof(float);
+    long unsigned int nbVec = nbElement/vecSize;
+    for (long unsigned int i = 0lu; i < nbVec; ++i) {
+        // tabResult = scal*tabX + tabY
+        __m256 vecX = _mm256_load_ps(tabX + i*vecSize);
+        __m256 vecY = _mm256_load_ps(tabY + i*vecSize);
+        __m256 vecAX = _mm256_mul_ps(vecX, vecScal);
+        __m256 vecRes = _mm256_add_ps(vecAX, vecY);
+        _mm256_store_ps(tabResult + i*vecSize, vecRes);
+    }
+}
+
+///Get the number of cycles per elements of the saxpy
+/**	@param nbElement : number of elements of the tables
+ * 	@param nbRepetition : number of repetition to evaluate the function saxpy
+*/
+void evaluateSaxpy(long unsigned int nbElement, long unsigned int nbRepetition)
+{
+    float *tabResult = (float *)asterics_malloc(sizeof(float) * nbElement);
+    float *tabX = (float *)asterics_malloc(sizeof(float) * nbElement);
+    float *tabY = (float *)asterics_malloc(sizeof(float) * nbElement);
+    float scal(4.0f);
+
+    for (long unsigned int i(0lu); i < nbElement; ++i)
+    {
+        tabX[i] = (float)(i * 32lu % 17lu);
+        tabY[i] = (float)(i * 57lu % 31lu);
+    }
+
+    long unsigned int beginTime(rdtsc());
+    for (long unsigned int i(0lu); i < nbRepetition; ++i)
+    {
+        saxpy(tabResult, scal, tabX, tabY, nbElement);
+    }
+    long unsigned int elapsedTime((double)(rdtsc() - beginTime) / ((double)nbRepetition));
+
+    double cyclePerElement(((double)elapsedTime) / ((double)nbElement));
+    cout << "evaluateSaxpy : nbElement = " << nbElement << ", cyclePerElement = " << cyclePerElement << " cy/el, elapsedTime = " << elapsedTime << " cy" << endl;
+    cerr << nbElement << "\t" << cyclePerElement << "\t" << elapsedTime << endl;
+
+    asterics_free(tabResult);
+    asterics_free(tabX);
+    asterics_free(tabY);
+}
+
+int main(int argc, char **argv)
+{
+    cout << "Saxpy" << endl;
+    evaluateSaxpy(1000lu, 1000000lu);
+    evaluateSaxpy(2000lu, 1000000lu);
+    evaluateSaxpy(3000lu, 1000000lu);
+    evaluateSaxpy(5000lu, 1000000lu);
+    evaluateSaxpy(10000lu, 1000000lu);
+    return 0;
+}
